3e7c5d2e5c6802af2e8c90a5fd50aee3085a7c81
Flow619 <88358248+Flow619@users.noreply.github.com>
2024-10-22 18:00:06 -0700

    Create SIOC221_HW2.m

---
+++ HW2/SIOC221_HW2.m
@@ -0,0 +1,14 @@
+%% SIOC 221A - HW2
+% Author: Trenton Saunders
+% Date: 10/22/2024
+
+%%
+close all;
+clear all;
+clc;
+
+%% Load: National Data Buoy Center Buoy 46047
+
+%% Plot Time Series
+% Visual evaluation. Plot the time series of wind speed, wave height, water temperature, and air temperature for both years.
+
