c4a8651789be034a663d8bd257445a1c1c4cd0de
Flow619 <88358248+Flow619@users.noreply.github.com>
2024-10-26 21:25:49 -0700

    Update SIOC221_HW2.m

--- HW2/SIOC221_HW2.m
+++ HW2/SIOC221_HW2.m
@@ -8,20 +8,20 @@ clear all;
 clc;
 
 %% Load: National Data Buoy Center Buoy 46047
-opts = detectImportOptions("DATA\46047h2022.txt\46047h2022.txt")
-opts.SelectedVariableNames = {opts.VariableNames{1:5},opts.VariableNames{7},opts.VariableNames{9},opts.VariableNames{14:15}}
+opts = detectImportOptions("DATA\46047h2022.txt\46047h2022.txt");
+opts.SelectedVariableNames = {opts.VariableNames{1:5},opts.VariableNames{7},opts.VariableNames{9},opts.VariableNames{14:15}};
 Data.h2022 = readtable("DATA\46047h2022.txt\46047h2022.txt",opts,"ReadVariableNames",true);
 
-opts = detectImportOptions("DATA\46047h2023.txt\46047h2023.txt")
-opts.SelectedVariableNames = {opts.VariableNames{1:5},opts.VariableNames{7},opts.VariableNames{9},opts.VariableNames{14:15}}
+opts = detectImportOptions("DATA\46047h2023.txt\46047h2023.txt");
+opts.SelectedVariableNames = {opts.VariableNames{1:5},opts.VariableNames{7},opts.VariableNames{9},opts.VariableNames{14:15}};
 Data.h2023 = readtable("DATA\46047h2023.txt\46047h2023.txt",opts,"ReadVariableNames",true);
 %% Plot Time Series
 % Visual evaluation. Plot the time series of wind speed, wave height, water temperature, and air temperature for both years.
-Data.h2022.Datenum = datenum(Data.h2022.x_YY,Data.h2022.MM,Data.h2022.DD,Data.h2022.hh,Data.h2022.mm, zeros(length(Data.h2022.mm),1) )
-Data.h2023.Datenum = datenum(Data.h2023.x_YY,Data.h2023.MM,Data.h2023.DD,Data.h2023.hh,Data.h2023.mm, zeros(length(Data.h2023.mm),1) )
+Data.h2022.Datenum = datenum(Data.h2022.x_YY,Data.h2022.MM,Data.h2022.DD,Data.h2022.hh,Data.h2022.mm, zeros(length(Data.h2022.mm),1) );
+Data.h2023.Datenum = datenum(Data.h2023.x_YY,Data.h2023.MM,Data.h2023.DD,Data.h2023.hh,Data.h2023.mm, zeros(length(Data.h2023.mm),1) );
 
-Data.h2022.Datetime = datetime(Data.h2022.Datenum,'ConvertFrom','datenum')
-Data.h2023.Datetime = datetime(Data.h2023.Datenum,'ConvertFrom','datenum')
+Data.h2022.Datetime = datetime(Data.h2022.Datenum,'ConvertFrom','datenum');
+Data.h2023.Datetime = datetime(Data.h2023.Datenum,'ConvertFrom','datenum');
 
 %% Plot Time Series (including bad data)
 
@@ -46,7 +46,7 @@ sgtitle('NDBC Buoy 46047','FontSize',20)
 
 %% QC Plot Time Series
 
-fig = figure
+fig = figure;
 subplot(4,1,1)
 hold on
 Mask2022 = Data.h2022.WSPD < 90;
@@ -99,37 +99,21 @@ YearMonth = datetime(YearMonth,'ConvertFrom','datenum');
 
 
 %% Least-squares fit (Q4) - Write this out as a function! Check with Classmates.
-omega_2022 = (2*pi)/(size(Data.h2022,1));
-omega_2023 = (2*pi)/(size(Data.h2023,1));
+omega_2022.annual = (2*pi)/(size(Data.h2022,1));
+omega_2023.annual = (2*pi)/(size(Data.h2023,1));
 
+[x_2022_annual.WSPD,x_2023_annual.WSPD,fig] = SIOC221A_LeastSquares_function(omega_2022,omega_2023,Data,Data.h2022.WSPD,Data.h2023.WSPD,90,fig,1);
+[x_2022_annual.WVHT,x_2023_annual.WVHT,fig] = SIOC221A_LeastSquares_function(omega_2022,omega_2023,Data,Data.h2022.WVHT,Data.h2023.WVHT,90,fig,2);
+[x_2022_annual.WTMP,x_2023_annual.WTMP,fig] = SIOC221A_LeastSquares_function(omega_2022,omega_2023,Data,Data.h2022.WTMP,Data.h2023.WTMP,900,fig,3);
+[x_2022_annual.ATMP,x_2023_annual.ATMP,fig] = SIOC221A_LeastSquares_function(omega_2022,omega_2023,Data,Data.h2022.ATMP,Data.h2023.ATMP,900,fig,4);
 
-time_2022 = 1:size(Data.h2022,1);
-time_2023 = 1:size(Data.h2023,1);
-
-y_2022 = Data.h2022.ATMP;
-y_2023 = Data.h2023.ATMP;
-
-time_2022(y_2022 > 90) = [];
-time_2023(y_2023 > 90) = [];
-
-Temp2022_Datetime = Data.h2022.Datetime;
-Temp2023_Datetime = Data.h2023.Datetime;
-
-Temp2022_Datetime(y_2022 > 90) = [];
-Temp2023_Datetime(y_2023 > 90) = [];
-
-y_2022(y_2022 > 90) = [];
-y_2023(y_2023 > 90) = [];
-
-
-A_2022 = [ones(1,length(time_2022));sin(omega_2022.*time_2022);cos(omega_2022.*time_2022)]';
-x_2022 = (inv(transpose(A_2022)*A_2022)) * (transpose(A_2022)*y_2022);
-
-A_2023 = [ones(1,length(time_2023));sin(omega_2023.*time_2023);cos(omega_2023.*time_2023)]';
-x_2023 = (inv(transpose(A_2023)*A_2023)) * (transpose(A_2023)*y_2023);
-
-y_bar_2022 = x_2022(1) + x_2022(2)*sin(omega_2022.*time_2022) + x_2022(3)*cos(omega_2022.*time_2022);
-y_bar_2023 = x_2023(1) + x_2023(2)*sin(omega_2023.*time_2023) + x_2023(3)*cos(omega_2023.*time_2023);
 
 
 %% Least-squares fit a semi-annual cycle (Q5)
+omega_2022.semiannual = (2*pi)/( size(Data.h2022,1) / 2 );
+omega_2023.semiannual = (2*pi)/( size(Data.h2023,1) / 2 );
+
+[x_2022_semiannual.WSPD,x_2023_semiannual.WSPD,fig] = SIOC221A_LeastSquares_function(omega_2022,omega_2023,Data,Data.h2022.WSPD,Data.h2023.WSPD,90,fig,1);
+[x_2022_semiannual.WVHT,x_2023_semiannual.WVHT,fig] = SIOC221A_LeastSquares_function(omega_2022,omega_2023,Data,Data.h2022.WVHT,Data.h2023.WVHT,90,fig,2);
+[x_2022_semiannual.WTMP,x_2023_semiannual.WTMP,fig] = SIOC221A_LeastSquares_function(omega_2022,omega_2023,Data,Data.h2022.WTMP,Data.h2023.WTMP,900,fig,3);
+[x_2022_semiannual.ATMP,x_2023_semiannual.ATMP,fig] = SIOC221A_LeastSquares_function(omega_2022,omega_2023,Data,Data.h2022.ATMP,Data.h2023.ATMP,900,fig,4);
