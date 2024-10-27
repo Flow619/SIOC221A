fbccc156116bc7ff170dde523a83ff3a8c2a5e45
Flow619 <88358248+Flow619@users.noreply.github.com>
2024-10-26 21:25:36 -0700

    Create SIOC221_HW2_DisplayValues_HW_writeup.m

---
+++ HW2/SIOC221_HW2_DisplayValues_HW_writeup.m
@@ -0,0 +1,76 @@
+%% Annual
+disp( '2022 - Wind Speed' ) 
+disp( ['Mean = ',num2str(x_2022_annual.WSPD(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2022_annual.WSPD(2)^2 + x_2022_annual.WSPD(3)^2 ) ) ] )
+
+disp( '2022 - Wave Height' ) 
+disp( ['Mean = ',num2str(x_2022_annual.WVHT(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2022_annual.WVHT(2)^2 + x_2022_annual.WVHT(3)^2 ) ) ] )
+
+disp( '2022 - Water Temperature' ) 
+disp( ['Mean = ',num2str(x_2022_annual.WTMP(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2022_annual.WTMP(2)^2 + x_2022_annual.WTMP(3)^2 ) ) ] )
+
+disp( '2022 - Air Temperature' ) 
+disp( ['Mean = ',num2str(x_2022_annual.ATMP(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2022_annual.ATMP(2)^2 + x_2022_annual.ATMP(3)^2 ) ) ] )
+
+
+disp( '2023 - Wind Speed' ) 
+disp( ['Mean = ',num2str(x_2023_annual.WSPD(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2023_annual.WSPD(2)^2 + x_2023_annual.WSPD(3)^2 ) ) ] )
+
+disp( '2023 - Wave Height' ) 
+disp( ['Mean = ',num2str(x_2023_annual.WVHT(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2023_annual.WVHT(2)^2 + x_2023_annual.WVHT(3)^2 ) ) ] )
+
+disp( '2023 - Water Temperature' ) 
+disp( ['Mean = ',num2str(x_2023_annual.WTMP(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2023_annual.WTMP(2)^2 + x_2023_annual.WTMP(3)^2 ) ) ] )
+
+disp( '2023 - Air Temperature' ) 
+disp( ['Mean = ',num2str(x_2023_annual.ATMP(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2023_annual.ATMP(2)^2 + x_2023_annual.ATMP(3)^2 ) ) ] )
+
+
+%% Semi-annual
+disp( '2022 - Wind Speed' ) 
+disp( ['Mean = ',num2str(x_2022_semiannual.WSPD(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2022_semiannual.WSPD(2)^2 + x_2022_semiannual.WSPD(3)^2 ) ) ] )
+disp( ['Semi-Annual Amplitude = ', num2str( sqrt(x_2022_semiannual.WSPD(4)^2 + x_2022_semiannual.WSPD(5)^2 ) ) ] )
+
+disp( '2022 - Wave Height' ) 
+disp( ['Mean = ',num2str(x_2022_semiannual.WVHT(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2022_semiannual.WVHT(2)^2 + x_2022_semiannual.WVHT(3)^2 ) ) ] )
+disp( ['Semi-Annual Amplitude = ', num2str( sqrt(x_2022_semiannual.WVHT(4)^2 + x_2022_semiannual.WVHT(5)^2 ) ) ] )
+
+disp( '2022 - Water Temperature' ) 
+disp( ['Mean = ',num2str(x_2022_semiannual.WTMP(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2022_semiannual.WTMP(2)^2 + x_2022_semiannual.WTMP(3)^2 ) ) ] )
+disp( ['Semi-Annual Amplitude = ', num2str( sqrt(x_2022_semiannual.WTMP(4)^2 + x_2022_semiannual.WTMP(5)^2 ) ) ] )
+
+disp( '2022 - Air Temperature' ) 
+disp( ['Mean = ',num2str(x_2022_semiannual.ATMP(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2022_semiannual.ATMP(2)^2 + x_2022_semiannual.ATMP(3)^2 ) ) ] )
+disp( ['Semi-Annual Amplitude = ', num2str( sqrt(x_2022_semiannual.ATMP(4)^2 + x_2022_semiannual.ATMP(5)^2 ) ) ] )
+
+
+disp( '2023 - Wind Speed' ) 
+disp( ['Mean = ',num2str(x_2023_semiannual.WSPD(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2023_semiannual.WSPD(2)^2 + x_2023_semiannual.WSPD(3)^2 ) ) ] )
+disp( ['Semi-Annual Amplitude = ', num2str( sqrt(x_2023_semiannual.WSPD(4)^2 + x_2023_semiannual.WSPD(5)^2 ) ) ] )
+
+disp( '2023 - Wave Height' ) 
+disp( ['Mean = ',num2str(x_2023_semiannual.WVHT(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2023_semiannual.WVHT(2)^2 + x_2023_semiannual.WVHT(3)^2 ) ) ] )
+disp( ['Semi-Annual Amplitude = ', num2str( sqrt(x_2023_semiannual.WVHT(4)^2 + x_2023_semiannual.WVHT(5)^2 ) ) ] )
+
+disp( '2023 - Water Temperature' ) 
+disp( ['Mean = ',num2str(x_2023_semiannual.WTMP(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2023_semiannual.WTMP(2)^2 + x_2023_semiannual.WTMP(3)^2 ) ) ] )
+disp( ['Semi-Annual Amplitude = ', num2str( sqrt(x_2023_semiannual.WTMP(4)^2 + x_2023_semiannual.WTMP(5)^2 ) ) ] )
+
+disp( '2023 - Air Temperature' ) 
+disp( ['Mean = ',num2str(x_2023_semiannual.ATMP(1))] ) 
+disp( ['Annual Amplitude = ', num2str( sqrt(x_2023_semiannual.ATMP(2)^2 + x_2023_semiannual.ATMP(3)^2 ) ) ] )
+disp( ['Semi-Annual Amplitude = ', num2str( sqrt(x_2023_semiannual.ATMP(4)^2 + x_2023_semiannual.ATMP(5)^2 ) ) ] )
