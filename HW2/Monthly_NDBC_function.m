db378408b7a467d78206a4d8d2a033310c343e71
Flow619 <88358248+Flow619@users.noreply.github.com>
2024-11-04 22:00:35 -0800

    add comments
    add comments to code

--- HW2/Monthly_NDBC_function.m
+++ HW2/Monthly_NDBC_function.m
@@ -3,12 +3,12 @@ function [DATA_Struct,fig] = Monthly_NDBC_function(Data,DATA_Struct,TimeSeries_V
 
 for i = 1:24
     if i <= 12
-        DATA_Struct{i,1} = Data.h2022.Datetime( Data.h2022.MM == i & TimeSeries_Variable_2022 < Threshold );
-        DATA_Struct{i,2} = TimeSeries_Variable_2022 ( Data.h2022.MM == i & TimeSeries_Variable_2022 < Threshold ) ;
-        DATA_Struct{i,3} = mean(DATA_Struct{i,2});
-        DATA_Struct{i,4} = std(DATA_Struct{i,2});
-        DATA_Struct{i,5} = length(DATA_Struct{i,2} );
-        DATA_Struct{i,6} = DATA_Struct{i,4} / (sqrt( DATA_Struct{i,5} / (7*24*6) ));
+        DATA_Struct{i,1} = Data.h2022.Datetime( Data.h2022.MM == i & TimeSeries_Variable_2022 < Threshold );    % Pull datetime from ith month (only good date; below threshold)
+        DATA_Struct{i,2} = TimeSeries_Variable_2022 ( Data.h2022.MM == i & TimeSeries_Variable_2022 < Threshold ) ;    % Pull data from ith month (only good date; below threshold)
+        DATA_Struct{i,3} = mean(DATA_Struct{i,2});  % take mean
+        DATA_Struct{i,4} = std(DATA_Struct{i,2});   % take std dev
+        DATA_Struct{i,5} = length(DATA_Struct{i,2} );   % num of observation in that month
+        DATA_Struct{i,6} = DATA_Struct{i,4} / (sqrt( DATA_Struct{i,5} / (7*24*6) ));   % Calculate standard error
 
         % plot( Data.Mean.WSPD{i,1},Data.Mean.WSPD{i,2},'.')
     else
--- HW2/SIOC221A_LeastSquares_function.m
+++ HW2/SIOC221A_LeastSquares_function.m
@@ -6,28 +6,28 @@ time_2023 = 1:size(Data.h2023,1);
 y_2022 = Var_2022;
 y_2023 = Var_2023;
 
-time_2022(y_2022 > Threshold) = [];
+time_2022(y_2022 > Threshold) = [];  % Remove bad data
 time_2023(y_2023 > Threshold) = [];
 
 Temp2022_Datetime = Data.h2022.Datetime;
 Temp2023_Datetime = Data.h2023.Datetime;
 
-Temp2022_Datetime(y_2022 > Threshold) = [];
+Temp2022_Datetime(y_2022 > Threshold) = [];  % remove bad data
 Temp2023_Datetime(y_2023 > Threshold) = [];
 
-y_2022(y_2022 > Threshold) = [];
+y_2022(y_2022 > Threshold) = [];   % remove bad data
 y_2023(y_2023 > Threshold) = [];
 
 %%
 if length(fieldnames(omega_2022)) == 1
 
-A_2022 = [ones(1,length(time_2022));sin(omega_2022.annual.*time_2022);cos(omega_2022.annual.*time_2022)]';
+A_2022 = [ones(1,length(time_2022));sin(omega_2022.annual.*time_2022);cos(omega_2022.annual.*time_2022)]'; % [1 sin(omega*t1) cos(omega*t1)]
 x_2022 = (inv(transpose(A_2022)*A_2022)) * (transpose(A_2022)*y_2022);
 
 A_2023 = [ones(1,length(time_2023));sin(omega_2023.annual.*time_2023);cos(omega_2023.annual.*time_2023)]';
-x_2023 = (inv(transpose(A_2023)*A_2023)) * (transpose(A_2023)*y_2023);
+x_2023 = (inv(transpose(A_2023)*A_2023)) * (transpose(A_2023)*y_2023);  % x = inv(transpose(A)*A) * transpose(A)*y
 
-y_bar_2022 = x_2022(1) + x_2022(2)*sin(omega_2022.annual.*time_2022) + x_2022(3)*cos(omega_2022.annual.*time_2022);
+y_bar_2022 = x_2022(1) + x_2022(2)*sin(omega_2022.annual.*time_2022) + x_2022(3)*cos(omega_2022.annual.*time_2022);  % Predicted Values (i.e., A*x)
 y_bar_2023 = x_2023(1) + x_2023(2)*sin(omega_2023.annual.*time_2023) + x_2023(3)*cos(omega_2023.annual.*time_2023);
 
 
@@ -40,7 +40,7 @@ plot(Temp2023_Datetime,y_bar_2023,'r','LineWidth',3)
 %%
 elseif length(fieldnames(omega_2022)) == 2
 
-A_2022 = [ones(1,length(time_2022)); sin(omega_2022.annual.*time_2022); cos(omega_2022.annual.*time_2022); sin(omega_2022.semiannual.*time_2022); cos(omega_2022.semiannual.*time_2022)]';
+A_2022 = [ones(1,length(time_2022)); sin(omega_2022.annual.*time_2022); cos(omega_2022.annual.*time_2022); sin(omega_2022.semiannual.*time_2022); cos(omega_2022.semiannual.*time_2022)]'; % [1 sin(omega1*t1) cos(omega1*t1) sin(omega2*t1) cos(omega2*t1)]
 x_2022 = (inv(transpose(A_2022)*A_2022)) * (transpose(A_2022)*y_2022);
 
 A_2023 = [ones(1,length(time_2023)); sin(omega_2023.annual.*time_2023); cos(omega_2023.annual.*time_2023); sin(omega_2023.semiannual.*time_2023); cos(omega_2023.semiannual.*time_2023)]';
