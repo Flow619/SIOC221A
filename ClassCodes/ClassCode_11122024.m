f0d035650056adba161d69d0bf54d68016ac406b
Flow619 <88358248+Flow619@users.noreply.github.com>
2024-11-14 21:42:43 -0800

    Update ClassCode_11122024.m

--- ClassCodes/ClassCode_11122024.m
+++ ClassCodes/ClassCode_11122024.m
@@ -11,14 +11,22 @@ load('C:\Users\Trenton\Downloads\midterm_dataset.mat')
 x = u;
 dt = time(2) - time(1);
 %% Pick the Number of segments
-Num_Segment = 2;
+Num_Segment = 8;
 
-%% Figure out how many segments fir in the time sereis.
+%% Figure out how many segments fit in the time sereis.
 
 if rem( length(time),Num_Segment) ~= 0
     disp('***User may want to pick another Num_Segment: Remainder***')
+    Segment_Length = floor(length(time)/Num_Segment);
+    Remainder = rem( length(time),Num_Segment);
 else
     Segment_Length = length(time)/Num_Segment;
+end
+
+%% Override Time Series if Necessary
+if exist('Remainder') == 1
+    x_old = x;
+    x = x(1:end-(Remainder));
 end
 
 %% Reshape your data into a matrix of segment length
@@ -31,6 +39,14 @@ end
 
 %% Average them together
 Mean_P = mean(P,2);
-%% Normalize Approprialty
-Normalize_Mean_P = Mean_P/
+
+%% Check Parseval's Theorem
+Parseval.LeftSide = mean(x.^2);  % Full Time-Series
+Parseval.RightSide = sum(Mean_P); % Why does this Work?!
+
 %% Make approriate freq vector
+
+
+%% figure
+figure
+loglog(freq,Mean_P,'c')
