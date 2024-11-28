fb62915c9cbf1336100fc45135609c7315aee7fb
Flow619 <88358248+Flow619@users.noreply.github.com>
2024-11-27 17:43:16 -0800

    Update SIOC221A_HW4_MainScript.m

--- HW4/SIOC221A_HW4_MainScript.m
+++ HW4/SIOC221A_HW4_MainScript.m
@@ -10,10 +10,20 @@ clc;
 %% 1. Load Data
 load('C:\Users\Trenton\Downloads\midterm_dataset.mat')
 
+%%
+dt = time(2)-time(1);
+
+%% Plot Full FFT
+[FullFFT,Freq_Full] = MySpectrum(u,dt,'ON');
+
 %% FFT Function
 % Function Inputs: time series, dt, and number of windows
 % Function Outputs: average spectrum, freq vector, error bars
-[Avg_P,freq,Error_Bars] = SpectralFunction(x,dt,Num_windows);
+[Mean_P,freq,Error_Bars] = SpectralFunction_2(u,dt,10);
+sum(Mean_P)*(freq(2)-freq(1))  % Parsevals
 
-%%
 
+%%  Plot
+hold on
+plot(freq,Mean_P,'r') % plot Mean Spectra
+errorbar(freq(end) + 10*(freq(2)-freq(1)) , Mean_P(end), ( 1 - Error_Bars(1)) * Mean_P(end) , (Error_Bars(2) - 1) * Mean_P(end),'r','LineWidth',2) % Plot Error Bars
